bca0c3188d4c501819105800af498ee99c70ec89
pedro-donoso <pedro.donoso.r@usach.cl>
2024-02-14 01:33:59 -0300

    creo sub menu

---
+++ app/tienda/layout.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function TiendaLayout({ children }) {
+  return (
+    <div>
+      <nav>
+        <h3>Sección Tienda</h3>
+        <ul>
+          <li>
+            <Link href='/tienda/categorias/'>Categorías</Link>
+          </li>
+          <li>
+            <Link href='/tienda/categorias/computadoras/'>Computadoras</Link>
+          </li>
+        </ul>
+      </nav>
+      {children}
+    </div>
+  );
+}
