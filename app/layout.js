411a887637d8e407ccd1d144f1d2a11a662793a5
pedro-donoso <pedro.donoso.r@usach.cl>
2024-02-14 00:39:16 -0300

    Creo navbar

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,17 @@
 export default function RootLayout({ children }) {
- return (
-    <html lang="es">
-      <body>{children}</body>
+  return (
+    <html>
+      <body>
+        <nav>
+          <h1>Navbar</h1>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Tienda</li>
+          </ul>
+        </nav>
+        {children}
+        </body>
     </html>
-  )
+  );
 }
