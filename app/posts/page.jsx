3a4e31f8643c513207e24097fa8006c4fad60337
pedro-donoso <pedro.donoso.r@usach.cl>
2024-02-14 15:00:56 -0300

    agrego colores

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -11,18 +11,42 @@ async function PostPages() {
   return (
     <div>
       {posts.map((post) => (
-        <div key={post.id}>
-          <h3>{post.id}. {post.title}</h3>
-          <p>
-            {post.description}
-          </p>
-          <img
-            src={post.image}
-            alt={post.title}
-            style={{ width: "200px", height: "200px" }}
-          />
-          <h3>${post.price}</h3>
-          <h3>{post.category}</h3>
+        <div
+          key={post.id}
+          style={{
+            border: "1px solid #0000ff",
+            padding: "20px",
+            margin: "10px",
+            background: "#ccc",
+          }}
+        >
+          <div style={{ color: "#0000ff" }}>
+            {post.id}. {post.title}
+          </div>
+  <div>
+            <img
+              src={post.image}
+              alt={post.title}
+              style={{
+                width: "150px",
+                height: "150px",
+                margin: "20px",
+              }}
+            />
+
+<div style={{color: "#404040"}}>
+   <p>{post.description}</p>
+</div>
+
+
+          </div>
+          <h3 style={{ margin: "10px", color: "#0000ff" }}>${post.price}</h3>
+          <h4 style={{ color: "#ff00ff" }}>Category: {post.category}</h4>
+
+        
+
+         
+
         </div>
       ))}
     </div>
