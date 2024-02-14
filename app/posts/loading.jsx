07c8c9b10fe5f726bc5be8e8923ceae8f9643cb0
pedro-donoso <pedro.donoso.r@usach.cl>
2024-02-14 16:28:47 -0300

    agrego loading

---
+++ app/posts/loading.jsx
@@ -0,0 +1,9 @@
+function LoadingPage() {
+    return (
+        <div>
+            <h2>Cargando Productos...</h2>
+        </div>
+    )
+}
+
+export default LoadingPage
--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -1,6 +1,9 @@
 async function loadPosts() {
   const res = await fetch("https://fakestoreapi.com/products");
   const data = await res.json();
+
+  
+
   return data;
 }
 
